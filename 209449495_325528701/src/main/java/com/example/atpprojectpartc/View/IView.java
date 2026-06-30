9d78a83407e0041f0e4b0b9d5d155ef44aa8bd60
lazaryu <yuval040201@gmail.com>
2026-06-30 21:44:11 +0300

    finish

--- 209449495_325528701/src/main/java/com/example/atpprojectpartc/View/IView.java
+++
@@ -1,9 +0,0 @@
-package com.example.atpprojectpartc.View;
-
-import com.example.atpprojectpartc.ViewModel.MyViewModel;
-
-public interface IView {
-    void setViewModel(MyViewModel viewModel);
-
-    void displayMessage(String message);
-}
